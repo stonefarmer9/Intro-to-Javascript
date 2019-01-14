0700d966a0589765d5efb2cb2b38a868b9b563bd
stonefarmer9 <g.p.levaillant@gmail.com>
2019-01-14 16:44:55 +0000

    Test passed - rock loses to paper

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -2,9 +2,11 @@ function rps (a, b) {
   if (a === 'rock' && b === 'scissors') {
     return "Rock wins"
   } else if ( a === 'paper' &&  b === 'rock') {
-      return 'Paper wins'
-    } else {
+    return 'Paper wins'
+  } else  if (a === 'scissors' && b === 'paper') {
     return "Scissors wins"
+  } else if (a === 'rock' && b === 'paper') {
+    return "Paper wins"
   }
 }
 module.exports = rps
